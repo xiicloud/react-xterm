9f6a707c47fc665de27260f7b6da1118341ba739
wowlusitong <lusitong@vip.qq.com>
2016-09-13 17:54:12 +0800

    add fullscreen support

--- src/Terminal.js
+++ src/Terminal.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {findDOMNode} from 'react-dom';
 import Xterm from 'xterm';
 import 'xterm/addons/fullscreen/fullscreen.js';
-import io from 'socket.io-client';
+import io from 'socket.io-client/socket.io.js';
 
 export default class Terminal extends React.Component {
   static propTypes = {
@@ -12,14 +12,14 @@ export default class Terminal extends React.Component {
     onError: React.PropTypes.func,
     onClose: React.PropTypes.func,
     title: React.PropTypes.string,
-    initialEmit: React.PropTypes.array
+    initialEmit: React.PropTypes.array,
+    fullscreen: React.PropTypes.bool
   }
 
   constructor(props) {
     super(props);
     this.term = new Xterm({cursorBlink: true});
-    this.path = props.socketURL || props.query.socketURL;
-    this.socket = this.createSocket(this.path);
+    this.socket = this.createSocket(props.socketURL);
     this.handleResize = this.handleResize.bind(this);
     this.width = props.width;
     this.height = props.height;
@@ -74,8 +74,8 @@ export default class Terminal extends React.Component {
   }
 
   componentWillMount() {
-    const onClose = this.props;
-    if (!this.path) {
+    const {onClose, socketURL} = this.props;
+    if (!socketURL) {
       onClose && onClose();
       this.close();
     }
@@ -85,7 +85,7 @@ export default class Terminal extends React.Component {
     const terminalContainer = findDOMNode(this);
     const {term} = this;
     term.open(terminalContainer);
-    this.props.query && term.toggleFullscreen();
+    this.props.fullscreen && term.toggleFullscreen();
     this.handleResize();
     window.addEventListener('resize', this.handleResize);
   }
@@ -123,5 +123,6 @@ Terminal.defaultProps = {
   width: 0,
   height: 0,
   title: '\x1b[32mWelcome to use cSphere online terminal!\x1b[m\r\n',
-  initialEmit: ['auth', 'terminal,50,50']
+  initialEmit: ['auth', 'terminal,50,50'],
+  fullscreen: false
 };
